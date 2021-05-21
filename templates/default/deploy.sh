d30df6760142a5fcfd0c2db33da70c212875f9a8
mfojtak <mfojtak@seznam.cz>
2021-05-21 11:47:04 +0000

    template location fixed

--- templates/default/deploy.sh
+++ templates/default/deploy.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 kubectl create -k ./
-kubectl wait --for=condition=complete job/{{ name }}-build --timeout 2h
+retval_complete=1
+retval_failed=1
+while [[ $retval_complete -ne 0 ]] && [[ $retval_failed -ne 0 ]]; do
+  sleep 5
+  output=$(kubectl wait --for=condition=failed job/{{ name }}-build --timeout=0 2>&1)
+  retval_failed=$?
+  output=$(kubectl wait --for=condition=complete job/{{ name }}-build --timeout=0 2>&1)
+  retval_complete=$?
+  kubectl logs -f job/{{ name }}-build
+done
+
+if [ $retval_failed -eq 0 ]; then
+    echo "Job failed. Please check logs."
+    exit 1
+fi
+echo "Image {{ image }} built successfully"
 kubectl apply -f deployment.yaml
